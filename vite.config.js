a746e0eb332fe4bbedc5b01aae9bb3ab7486360f
yapisango <yapisango@gmail.com>
2024-12-13 11:58:49 +0200

    Updated
    Resolved some compatibility issues with React 19 and React Mackup.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import {defineConfig} from "vite"
 import react from "@vitejs/plugin-react"
+import process from 'process';
 
 export default defineConfig({
 	plugins: [
 		react()
-	]
-})
+	],
+  	server: {
+    port: 3000,
+    },
+	define: {
+    'process.env': {},
+   },
+  	resolve: {
+    alias: {
+      process: 'process/browser',
+    },
+  },
+});
+
