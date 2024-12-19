650da3130379351d11fb124aeba037eeef908419
yapisango <yapisango@gmail.com>
2024-12-19 14:30:56 +0200

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,8 @@
 import {defineConfig} from "vite"
 import react from "@vitejs/plugin-react"
-import process from 'process';
 
 export default defineConfig({
 	plugins: [
 		react()
-	],
-  	server: {
-    port: 3000,
-    },
-	define: {
-    'process.env': {},
-   },
-  	resolve: {
-    alias: {
-      process: 'process/browser',
-    },
-  },
-});
-
+	]
+})
